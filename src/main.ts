cf7bf28d56a936432474769b9a367b565596a31a
Ben <ben83026@outlook.com>
2023-10-22 00:25:48 +1100

    explore TS object defining

--- src/main.ts
+++ src/main.ts
@@ -80,3 +80,85 @@ mixed = myTuple
 //1. Type '42' is not assignable to type 'undefined'.
 //2. Tuple type '[string, number, boolean]' of length '3' has no element at index '3'.
 myTuple[1] = 42
+
+
+//objects
+let myObj: object
+// since array is also a type of object
+myObj = []
+// The log result is object
+console.log(typeof myObj)
+//bands is a string array so it's ok to assign to myObj
+myObj = bands
+//And it's ok to assign an empty pairs curly brace
+myObj = {}
+
+const exampleObj = {
+    prop1: "Ben",
+    prop2: true
+}
+// Because the type of prop1 was assigned with type
+exampleObj.prop1 = "Benny"
+
+type Guitarist = {
+    name: string,
+    //only in Type Object definition can add a question mark behind
+    //attributes to define it to be the following type or undefined
+    //optional
+    active?: boolean,
+    // an array of union type of string and number only
+    albums: (string | number)[]
+}
+
+
+let evh: Guitarist = {
+    name: "Eddie",
+    //because the active in type definition is either boolean or undefined
+    //active: false,
+    albums: [1984, 5150, 'OU812']
+}
+
+let jp: Guitarist = {
+    name: "Jimmy",
+    active: true,
+    albums: ['I', 'II', 'IV']
+}
+// Beacuse they are both Guitarist type
+evh = jp
+
+// add properties through evh.years = 40 is not going to work
+// because the type properties are already predefined
+
+const greetGuitarist = (guitarist: Guitarist) => {
+    return `Hello, ${guitarist.name}!`
+}
+
+console.log(greetGuitarist(jp))
+
+// For type defining form we can use interface as well
+// but no equal sign with interface
+// interface would normally be used when it's defining object
+// like a class
+interface Member {
+    name?: string
+    no: Number
+}
+
+let member1: Member = {
+    name: "Anne",
+    no: 1
+}
+
+const greetMember = (member1: Member) => {
+    if (member1.name) {
+        // If apply a method on the undefined property, TS will
+        // automatically add a question mark behind the property
+        // To apply the condition check without question can remove the
+        // question mark
+        return `Hello, ${member1.name.toUpperCase()}!`
+        // return `Hello, ${member1.name?.toUpperCase()}!`
+    }
+    //because it's return a union type :
+    //const greetMember: (member1: Member) => string | undefined
+    return 'Hello'
+}
