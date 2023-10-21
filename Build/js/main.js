fdcbbd28f44bd403175cdef99361dc06fbd148ef
Ben <ben83026@outlook.com>
2023-10-21 13:24:49 +1100

    setup basic typescript

---
+++ Build/js/main.js
@@ -0,0 +1,8 @@
+"use strict";
+let yourname = "Ben";
+console.log(yourname);
+let a = 12;
+let b = "6";
+let c = 2;
+console.log(a / b);
+console.log(c * b);
---
+++ src/main.ts
@@ -0,0 +1,12 @@
+let yourname = "Ben"
+console.log(yourname)
+
+
+let a: number = 12
+let b: number = 6
+// let b: string = "6"will throw error
+let c: number = 2
+
+console.log(a / b)
+
+console.log(c * b)
