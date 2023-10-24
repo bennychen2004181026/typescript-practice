91798a6d2a909fead7bfca4beb9ce3833962a4e6
Ben <ben83026@outlook.com>
2023-10-24 20:32:06 +1100

    TS Generics

--- Build/js/main4.js
+++ Build/js/main4.js
@@ -1,1 +1,85 @@
 "use strict";
+// Index Signatures
+const todaysTrasactions = {
+    Pizza: -10,
+    Books: -5,
+    Job: 50
+};
+console.log(todaysTrasactions.Pizza); //-10
+console.log(todaysTrasactions['Pizza']); //-10
+let prop = 'Pizza';
+console.log(todaysTrasactions[prop]); //No index signature with a parameter of type 'string' was found on type 'TransactionObg'.
+const todayNet = (transactions) => {
+    let total = 0;
+    for (const transaction in transactions) {
+        //with Index Signatures being defined above, it does not appear warning in TS
+        total += transactions[transaction];
+    }
+    return total;
+};
+console.log(todayNet(todaysTrasactions));
+//Index signature in type 'TransactionObg' only permits reading.
+// todaysTrasactions.Pizza = 40
+//TS can not see if the property exists
+console.log(todaysTrasactions['Ben']); //undefined
+//create an object student from Student inderface
+const student = {
+    name: 'Ben',
+    GPA: 3.5,
+    classes: [100, 200]
+};
+// Because we define the interface with optional property with undefined,so TS does not know if the specific property exist
+//console.log(student.test)
+for (const key in student) {
+    // if the above Index signature is not defined in interface, then we use assertion as to notice TS
+    //the keyof here is for creating a union type of above
+    console.log(`${key}:${student[key]}`);
+}
+Object.keys(student).map(key => {
+    //use typeof to inform TS what the type is for specific property
+    //becasu we are using Object not the interface, so is the student not the Student
+    console.log(student[key]);
+});
+//Because we don't return any here so use the void type.
+// keyof will be defined as string literal
+const logStudentKey = (student, key) => {
+    console.log(`Student ${key}: ${student[key]}`);
+};
+logStudentKey(student, 'name'); //Student name: Ben
+const monthlyIncomes = {
+    salary: 20000,
+    bounus: 2000,
+    sidehustle: 600
+};
+for (const revenue in monthlyIncomes) {
+    console.log(monthlyIncomes[revenue]);
+}
+const users1 = {
+    '1': 'Alice',
+    '2': 'Bob',
+    '3': 'Charlie'
+};
+// This is equivalent to
+const users = {
+    '1': 'Alice',
+    '2': 'Bob',
+    '3': 'Charlie'
+};
+//2: Mapping Enum to Values
+var Fruit;
+(function (Fruit) {
+    Fruit["Apple"] = "apple";
+    Fruit["Orange"] = "orange";
+    Fruit["Mango"] = "mango";
+})(Fruit || (Fruit = {}));
+const colors = {
+    [Fruit.Apple]: 'red',
+    [Fruit.Orange]: 'orange',
+    [Fruit.Mango]: 'yellow'
+};
+//In this example, PersonRecord will have all the same keys as Person, but each property will be of type string.
+const personRecord = {
+    name: 'Alice',
+    age: '30',
+    address: 'Wonderland'
+};
---
+++ src/main5.ts
@@ -0,0 +1,157 @@
+// Generics
+// Generics allows you to write reusable and flexible code without sacrificing type safety. With generics, you can write functions and classes that work with different types while still retaining the type information.
+
+// const stringEcho = (arg:string):string =>arg
+// It does not only work with string type, and now it work with any type that pass into this function 
+const echo = <T>(arg: T): T => arg
+
+const isObj = <T>(arg: T): boolean => {
+    //checking three things here:1.is an object. 2.is not an array. 3. is not null
+    return (typeof arg === 'object' && !Array.isArray(arg) && arg !== null)
+}
+
+console.log(isObj(true))//false
+console.log(isObj('John'))//false
+console.log(isObj([1, 2, 3]))//false
+console.log(isObj({ name: 'John' }))//true
+console.log(isObj(null))//false
+
+const isTrue = <T>(arg: T): { arg: T, is: boolean } => {
+    // check if the array is empty as well
+    if (Array.isArray(arg) && !arg.length) {
+        //double exclamation mark is a shorthand way to convert any value to its boolean representation in JavaScript and TypeScript. 
+        return { arg, is: false }
+    }
+    // check if the object is empty as well
+    if (isObj(arg) && !Object.keys(arg as keyof T).length) {
+        return { arg, is: false }
+    }
+    return { arg, is: !!arg }
+
+}
+
+console.log(isTrue(false))//{arg:false,is:false}
+console.log(isTrue(0))//{arg:0,is:false}
+console.log(isTrue(true))//{arg:true,is:true}
+console.log(isTrue(1))//{arg:1,is:true}
+console.log(isTrue('Ben'))//{arg:'Ben,is:true}
+console.log(isTrue(''))//{arg:'',is:false}
+console.log(isTrue(null))//{arg:null,is:false}
+console.log(isTrue(undefined))//{arg:undefined,is:false}
+console.log(isTrue({}))//{arg:{},is:false}
+console.log(isTrue({ name: 'Ben' }))//{arg:{name:'Ben'},is:true}
+console.log(isTrue([]))//{arg:[],is:false}
+console.log(isTrue([1, 2, 3]))//{arg:[1,2,3],is:true}
+console.log(isTrue(NaN))//{arg:NaN,is:false}
+console.log(isTrue(-0))//{arg:false,is:false}
+
+interface BoolCheck<T> {
+    value: T,
+    is: boolean
+}
+
+const checkBoolValue = <T>(arg: T): BoolCheck<T> => {
+    if (Array.isArray(arg) && !arg.length) {
+        return { value: arg, is: false }
+    }
+    if (isObj(arg) && !Object.keys(arg as keyof T).length) {
+        return { value: arg, is: false }
+    }
+    return { value: arg, is: !!arg }
+}
+
+interface HasID {
+    id: number
+}
+
+const processUser = <T extends HasID>(user: T): T => {
+    //process the user with logic here
+    return user
+}
+
+console.log(processUser({ id: 1, name: 'Ben' }))
+//Object literal may only specify known properties, and 'name' does not exist in type 'HasID'.
+// console.log(processUser({name:'Ben'}))
+
+
+//T is similar to an user object here,K will be the keys of T of that user object
+const getuserProperty = <T extends HasID, K extends keyof T>(users: T[], key: K): T[K][] => {
+    return users.map(user => user[key])
+}
+
+const userArray = [
+    {
+        'id': 1,
+        'name': 'Leane Graham',
+        'username': 'Bret',
+        'email': 'Sincere@april.biz',
+        'address': {
+            'street': 'Kulas light',
+            'suite': 'Apt. 556',
+            'city': 'Gwenborough',
+            'zipcode': '92998-3874',
+            'geo': {
+                'lat': '-37.3159',
+                'lng': '81.1496'
+            }
+        },
+        'phone': '1-770-736 x56442',
+        'website': 'hildegard.org',
+        'company': {
+            'name': 'Deckow-Crist',
+            'catchPhrase': 'Multi-layered client-server neural-net',
+            'bs': 'harness real-time e-markets'
+        }
+    },
+
+    {
+        'id': 2,
+        'name': 'Ervin Howell',
+        'username': 'Antonette',
+        'email': 'Shanna@elissa.tv',
+        'address': {
+            'street': 'Victor Plains',
+            'suite': 'Suite 879',
+            'city': 'Wisokyburgh',
+            'zipcode': '90566-7771',
+            'geo': {
+                'lat': '-43.9509',
+                'lng': '-34.4618'
+            }
+        },
+        'phone': '101-692-6593 x09125',
+        'website': 'anastasia.net',
+        'company': {
+            'name': 'Deckow-Crist',
+            'catchPhrase': 'Proactive didactic contingency',
+            'bs': 'synergize scalable supply-chains'
+        }
+    }
+]
+
+console.log(getuserProperty(userArray, 'email'))//['Sincere@april.biz','Shanna@elissa.tv']
+
+console.log(getuserProperty(userArray, 'username'))//['Bret','Antonette']
+
+class StateObject<T> {
+    private data: T
+    constructor(value: T) {
+        this.data = value
+    }
+    get state(): T {
+        return this.data
+    }
+    set state(value: T) {
+        this.data = value
+    }
+}
+//we can specify the type by add <string> right behind the StateObject
+const store = new StateObject('John')
+console.log(store.state)//John
+store.state = 'Dave'
+// store.state = 12//Type 'number' is not assignable to type 'string'.Because we assigned a string to state when creating this object and is not going to accept another type of state
+
+const myState = new StateObject<(string | number | boolean)[]>([15])
+
+myState.state = (['Dave', 42, true])
+console.log(myState.state)//['Dave',42,true]
